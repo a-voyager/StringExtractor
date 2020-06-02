85b7c7418a07d531486acde9eb936be7d547b715
voyagerwu <voyagerwu@tencent.com>
2020-06-02 23:15:01 +0800

    feature: add repeat key value support

--- src/top/wuhaojie/se/process/StringsWriter.kt
+++ src/top/wuhaojie/se/process/StringsWriter.kt
@@ -2,18 +2,24 @@ package top.wuhaojie.se.process
 
 import com.intellij.lang.xml.XMLLanguage
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiManager
 import com.intellij.psi.XmlElementFactory
+import com.intellij.psi.impl.source.xml.XmlElementImpl
+import com.intellij.psi.impl.source.xml.XmlTagImpl
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.xml.XmlComment
-import com.intellij.psi.xml.XmlFile
-import com.intellij.psi.xml.XmlTag
+import com.intellij.psi.util.elementType
+import com.intellij.psi.xml.*
+import org.jetbrains.rpc.LOG
 import top.wuhaojie.se.entity.TaskHolder
+import top.wuhaojie.se.utils.Log
 
 class StringsWriter(
         private val project: Project
 ) : AbsWriter() {
 
+    private val stringResourceMap = hashMapOf<String, String>()
 
     private fun openStringsFile(taskHolder: TaskHolder): XmlFile? {
         val virtualFile = taskHolder.desFile ?: return null
@@ -30,8 +36,15 @@ class StringsWriter(
     private fun writeContent(rootTag: XmlTag, taskHolder: TaskHolder) {
         val fields = taskHolder.selectedFields()
         for (field in fields) {
-            val childTag = rootTag.createChildTag("string", "", field.source, false)
-            childTag.setAttribute("name", field.result)
+            val resKey = field.result
+            val resValue = field.source
+            if (stringResourceMap.containsKey(resKey) && stringResourceMap[resKey] == resValue) {
+                Log.d("repeat string key and value ", resKey, resValue)
+                return
+            }
+            stringResourceMap[resKey] = resValue
+            val childTag = rootTag.createChildTag("string", "", resValue, false)
+            childTag.setAttribute("name", resKey)
             rootTag.add(childTag)
         }
     }
@@ -44,6 +57,25 @@ class StringsWriter(
         saveAllFile()
         val xmlFile = openStringsFile(taskHolder) ?: return
         val rootTag = xmlFile.rootTag ?: return
+
+        rootTag.acceptChildren(object : PsiElementVisitor() {
+
+            override fun visitElement(element: PsiElement) {
+                super.visitElement(element)
+                if (element.elementType != XmlElementType.XML_TAG) {
+                    return
+                }
+                val xmlTag = element as XmlTag
+                val attribute = xmlTag.attributes.find { it.name == "name" } ?: return
+                val resKey = attribute.value.toString()
+                val resValue = xmlTag.value.trimmedText
+                stringResourceMap[resKey] = resValue
+            }
+
+        })
+
+        Log.d("existed string", stringResourceMap)
+
 //        writeComment(rootTag, taskHolder.descTag)
         writeContent(rootTag, taskHolder)
         saveAllFile()
