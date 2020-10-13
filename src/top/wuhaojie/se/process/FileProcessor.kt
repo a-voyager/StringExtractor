d5955438c034f5ab0e7787508c62ca19473ae3ce
voyagerwu <voyagerwu@tencent.com>
2020-10-13 09:37:11 +0800

    [fix] strings.xml output path fixed

--- src/top/wuhaojie/se/process/FileProcessor.kt
+++ src/top/wuhaojie/se/process/FileProcessor.kt
@@ -1,9 +1,12 @@
 package top.wuhaojie.se.process
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.psi.PsiFile
 import top.wuhaojie.se.entity.TaskHolder
+import java.io.File
 
 object FileProcessor {
 
@@ -15,8 +18,10 @@ object FileProcessor {
         val virtualFile = psiFile.virtualFile
         taskHolder.currentFile = virtualFile
         val module = ProjectRootManager.getInstance(project).fileIndex.getModuleForFile(virtualFile) ?: return
-        val moduleFile = module.moduleFile ?: return
-        val desFile = moduleFile.parent.findFileByRelativePath(defaultFilePath) ?: return
+        val absolutePath = File(ModuleRootManager.getInstance(module).contentRoots[0].path, defaultFilePath).absolutePath
+        val desFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(absolutePath)
+        // val moduleFile = module.moduleFile ?: return
+        // val desFile = moduleFile.parent.findFileByRelativePath(defaultFilePath) ?: return
         taskHolder.desFile = desFile
     }
 
